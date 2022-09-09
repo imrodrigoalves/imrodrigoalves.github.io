c654a34c3228118caca5afde51a4d53148b55e77
Rodrigo Alves <23285553+imrodrigoalves@users.noreply.github.com>
2022-09-09 16:09:23 +0100

    .

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,10 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    target: 'es2015',
+    outDir: './imrodrigoalves.github.io'
+ },
   plugins: [vue()],
   resolve: {
     alias: {
